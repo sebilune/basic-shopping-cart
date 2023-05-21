e974b7c3425a4f7aae84f91e93a298284a05c646
Sebastian Ramirez <sebastianramord@gmail.com>
2023-05-20 22:35:55 -0500

    Made changes to script.js Fixed a bug where the user could input nothing and log it to the list of items. Now, the user must input at least a character for the shopping cart to work.

--- script.js
+++ script.js
@@ -3,22 +3,25 @@ const input = document.querySelector("input");
 const btn = document.querySelector("button");
 
 function addItem() {
-  let inputValue = input.value;
-  input.value = "";
-
-  const li = document.createElement("li");
-  const span = document.createElement("span");
-  const btnTwo = document.createElement("button");
-
-  span.textContent = inputValue;
-  btnTwo.textContent = "Remove ⛔";
-
-  ul.appendChild(li);
-  li.appendChild(span);
-  li.appendChild(btnTwo);
-
-  btnTwo.addEventListener("click", () => li.remove());
-
+  if (input.value === "") {
+    alert("Please enter a value.");
+  } else {
+    let inputValue = input.value;
+    input.value = "";
+  
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const btnTwo = document.createElement("button");
+  
+    span.textContent = inputValue;
+    btnTwo.textContent = "Remove ⛔";
+  
+    ul.appendChild(li);
+    li.appendChild(span);
+    li.appendChild(btnTwo);
+  
+    btnTwo.addEventListener("click", () => li.remove());
+  }
   input.focus();
 }
 
