374fa617f5c90a30993b26ffec7d0ea91a4d2d05
Sebastian Ramirez <sebastianramord@gmail.com>
2023-05-20 21:03:39 -0500

    Init files.

---
+++ script.js
@@ -0,0 +1,25 @@
+const ul = document.querySelector("ul");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+
+function addItem() {
+  let inputValue = input.value;
+  input.value = "";
+
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const btnTwo = document.createElement("button");
+
+  span.textContent = inputValue;
+  btnTwo.textContent = "Delete Item";
+
+  ul.appendChild(li);
+  li.appendChild(span);
+  li.appendChild(btnTwo);
+
+  btnTwo.addEventListener("click", () => li.remove());
+
+  input.focus();
+}
+
+btn.addEventListener("click", addItem);
